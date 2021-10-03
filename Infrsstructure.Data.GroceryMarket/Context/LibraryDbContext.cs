710e3cc9198efefb990e1bca4981ae422dc87297
Tatjana <tance_m@hotmail.com>
2021-10-03 10:10:19 +0200

    archetecture and  dbContext buld

---
+++ Infrsstructure.Data.GroceryMarket/Context/LibraryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data.GroceryMarket.Context
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base (options)
+        {
+
+        }
+
+    }
+}
