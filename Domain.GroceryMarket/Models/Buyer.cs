f23db127557202790e676b25313a6b5c25e8b322
Tatjana <tance_m@hotmail.com>
2021-10-06 16:51:55 +0200

    model buyer class added

---
+++ Domain.GroceryMarket/Models/Buyer.cs
@@ -0,0 +1,16 @@
+﻿using Domain.GroceryMarket.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.GroceryMarket.Models
+{
+    public class Buyer : AuditableBaseEntity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
