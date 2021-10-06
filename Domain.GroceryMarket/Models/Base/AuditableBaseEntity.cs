8ff2bada89597f213a7dab7a86ac430c9cbf5156
Tatjana <tance_m@hotmail.com>
2021-10-06 13:53:25 +0200

    vegetables and fruits models created

---
+++ Domain.GroceryMarket/Models/Base/AuditableBaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.GroceryMarket.Models.Base
+{
+    public abstract class AuditableBaseEntity : BaseEntity
+    {
+        public virtual string CreatedBy { get; set; }
+        public virtual DateTime Created { get; set; }
+        public virtual string LastModifiedBy { get; set; }
+        public virtual DateTime LastModified { get; set; }
+    }
+}
---
+++ Domain.GroceryMarket/Models/Fruits.cs
@@ -0,0 +1,14 @@
+﻿using Domain.GroceryMarket.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.GroceryMarket.Models
+{
+    public class Fruits : AuditableBaseEntity
+    {
+        public string Name { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public int InStock { get; set; }
+    }
+}
